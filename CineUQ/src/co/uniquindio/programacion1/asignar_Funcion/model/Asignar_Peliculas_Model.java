f8ee82b6ff4428913f569212e66ad9c5e1447d17
Ferney Cardona <ferneycardonagarcia110@gmail.com>
2022-10-11 15:57:29 -0500

    Subi imagen

--- CineUQ/src/co/uniquindio/programacion1/asignar_Funcion/model/Asignar_Peliculas_Model.java
+++ CineUQ/src/co/uniquindio/programacion1/asignar_Funcion/model/Asignar_Peliculas_Model.java
@@ -7,33 +7,35 @@ public class Asignar_Peliculas_Model {
 	
 	public ArrayList getFuncion () {
 		ArrayList<ArrayList<String>> funciones= new ArrayList<ArrayList<String>>();
-		String laHuerfana = "La Huerfana\n120 min\nClasificacion +15\nSinopsis*imagen.jpg";
-		String elLobo = "El Lobo\n150 min\nClasificacion +18\nSinopsis*imagen.jpg";
-		String spider = "SpiderMan\\n200 min\\nClasificacion +18\\nSinopsis*imagen.jpg";
+		String laHuerfana = "La Huérfana: El Origen\n120 min\nClasificacion +15\nSinopsis: La desquiciada Leena Klammer organiza una brillante fuga de un manicomio de Estonia y viaja a Estados Unidos robando la identidad de la hija desaparecida de una familia adinerada. Sin embargo, la nueva vida de Leena como \"Esther\" llega con un giro inesperado y la enfrenta a una madre que hará cualquier cosa para proteger a su familia a toda costa.";
+		String elLobo = "El Lobo\n150 min\nClasificacion +18\nSinopsis";
+		String spider = "SpiderMan\\n200 min\\nClasificacion +18\\nSinopsis";
 		String tres = "3:00 PM";
 		String cinco = "5:00 PM";
 		String ocho = "8:00 PM";
 		
-		ArrayList<String> funcionX = new ArrayList<String>();
-		funcionX.add("2022/10/06");
-		funcionX.add(tres);
-		funcionX.add(laHuerfana);
-		
-		ArrayList<String> funcionY = new ArrayList<String>();
-		funcionX.add("2022/10/06");
-		funcionX.add(cinco);
-		funcionX.add(elLobo);
+		ArrayList<String> funcion1 = new ArrayList<String>();
+		funcion1.add("2022/10/06");
+		funcion1.add(tres);
+		funcion1.add(laHuerfana);
+		funcion1.add("laHuerfana.jpg");
 		
 		
-		ArrayList<String> funcionZ = new ArrayList<String>();
-		funcionZ.add("2022/10/06");
-		funcionZ.add(ocho);
-		funcionZ.add(elLobo);
+		ArrayList<String> funcion2 = new ArrayList<String>();
+		funcion2.add("2022/10/06");
+		funcion2.add(cinco);
+		funcion2.add(elLobo);
 		
 		
-		funciones.add(funcionX);
-		funciones.add(funcionY);	
-		funciones.add(funcionZ);	
+		ArrayList<String> funcion3 = new ArrayList<String>();
+		funcion3.add("2022/10/06");
+		funcion3.add(ocho);
+		funcion3.add(elLobo);
+		
+		
+		funciones.add(funcion1);
+		funciones.add(funcion2);	
+		funciones.add(funcion3);	
 
 		System.out.print(funciones);
 
