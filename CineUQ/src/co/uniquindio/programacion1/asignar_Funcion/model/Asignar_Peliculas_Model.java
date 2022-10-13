111d587868eab76351fbfa5f798e55876a34546d
Ferney Cardona <ferneycardonagarcia110@gmail.com>
2022-10-13 17:10:51 -0500

    Cambios de arreglos y metodos y agg peliculas

--- CineUQ/src/co/uniquindio/programacion1/asignar_Funcion/model/Asignar_Peliculas_Model.java
+++ CineUQ/src/co/uniquindio/programacion1/asignar_Funcion/model/Asignar_Peliculas_Model.java
@@ -7,40 +7,49 @@ public class Asignar_Peliculas_Model {
 	
 	public ArrayList getFuncion () {
 		ArrayList<ArrayList<String>> funciones= new ArrayList<ArrayList<String>>();
-		String laHuerfana = "La Huérfana: El Origen\n120 min\nClasificacion +15\nSinopsis: La desquiciada Leena Klammer organiza una brillante fuga de un manicomio de Estonia y viaja a Estados Unidos robando la identidad de la hija desaparecida de una familia adinerada. Sin embargo, la nueva vida de Leena como \"Esther\" llega con un giro inesperado y la enfrenta a una madre que hará cualquier cosa para proteger a su familia a toda costa.";
-		String elLobo = "El Lobo\n150 min\nClasificacion +18\nSinopsis";
-		String spider = "SpiderMan\\n200 min\\nClasificacion +18\\nSinopsis";
+		String laHuerfana = "La Huérfana: El Origen, 120 Min, Clasificacion +15, Sinopsis: La desquiciada Leena Klammer organiza una brillante fuga de un manicomio de Estonia y viaja a Estados Unidos robando la identidad de la hija.";
+		String tadeo = "Tadeo Jones 3, 90 Min, Clasificacion +7, Sinopsis: El sueño más grande de Tad es ser aceptado por sus colegas arqueólogos, pero su naturaleza accidentada se interpone. Cuando él, inadvertidamente, destruye un sarcófago extraño, se conjura un hechizo que pone en peligro la vida de sus amigos.";
+		String superMascotas = "Súper-Mascotas, 110 Min, Clasificacion +7, Sinopsis: Cuando la Liga de la Justicia es capturada por Lex Luthor, el perro de Superman, Krypto, forma un equipo de mascotas de refugio a las que se les otorgan superpoderes.";
 		String tres = "3:00 PM";
 		String cinco = "5:00 PM";
 		String ocho = "8:00 PM";
 		
 		ArrayList<String> funcion1 = new ArrayList<String>();
-		funcion1.add("2022/10/06");
+		funcion1.add("2022-10-13");
+		funcion1.add("0");
 		funcion1.add(tres);
 		funcion1.add(laHuerfana);
 		funcion1.add("laHuerfana.jpg");
 		
 		
 		ArrayList<String> funcion2 = new ArrayList<String>();
-		funcion2.add("2022/10/06");
+		funcion2.add("2022-10-13");
+		funcion2.add("1");
 		funcion2.add(cinco);
-		funcion2.add(elLobo);
+		funcion2.add(tadeo);
+		funcion2.add("tadeo.jpg");
+		
 		
 		
 		ArrayList<String> funcion3 = new ArrayList<String>();
 		funcion3.add("2022/10/06");
+		funcion3.add("2");
 		funcion3.add(ocho);
-		funcion3.add(elLobo);
+		funcion3.add(superMascotas);
+		funcion3.add("supermascotas.jpg");
+		
+	
 		
 		
 		funciones.add(funcion1);
 		funciones.add(funcion2);	
 		funciones.add(funcion3);	
 
-		System.out.print(funciones);
-
 		return funciones;
 	}
+	
+	
+	
 	
 	
 	
--- CineUQ/src/co/uniquindio/programacion1/asignar_Funcion/view/Ventana_AsignarPelicula.java
+++ CineUQ/src/co/uniquindio/programacion1/asignar_Funcion/view/Ventana_AsignarPelicula.java
@@ -1,32 +1,28 @@
 package co.uniquindio.programacion1.asignar_Funcion.view;
 
-import java.awt.Color;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import co.uniquindio.programacion1.asignar_Funcion.model.Asignar_Peliculas_Model;
-
 import java.awt.GridBagLayout;
 import javax.swing.JButton;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
-import java.awt.BorderLayout;
-import java.awt.Font;
-import javax.swing.JToggleButton;
-import java.awt.CardLayout;
-import javax.swing.BoxLayout;
+
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+
 
 public class Ventana_AsignarPelicula extends JFrame {
 
 	private JPanel contentPane;
 	private Asignar_Peliculas_Model asignarPelicula;
+	LocalDate fechahoy = LocalDate.now();
 
 	/**
 	 * Launch the application.
@@ -51,6 +47,8 @@ public class Ventana_AsignarPelicula extends JFrame {
 	public Ventana_AsignarPelicula() {
 		asignarPelicula = new Asignar_Peliculas_Model();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+
 		setSize(500,500);
 		setBounds(700, 700, 1000, 700);
 		contentPane = new JPanel();
@@ -59,62 +57,131 @@ public class Ventana_AsignarPelicula extends JFrame {
 		contentPane.setLayout(null);
 		
 		
-		
+
+
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
 		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        
 		
-		
-		
-		
-//
-		
-		JButton pelicula1 = new JButton("");
-		pelicula1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				
+		getPeli(((ArrayList<?>) asignarPelicula.getFuncion().get(0)).get(4), ((ArrayList<?>) asignarPelicula.getFuncion().get(0)).get(3).toString(),  ((ArrayList<?>) asignarPelicula.getFuncion().get(0)).get(1).toString());
+		getPeli(((ArrayList<?>) asignarPelicula.getFuncion().get(1)).get(4), ((ArrayList<?>) asignarPelicula.getFuncion().get(1)).get(3).toString(),  ((ArrayList<?>) asignarPelicula.getFuncion().get(1)).get(1).toString());
+		getPeli(((ArrayList<?>) asignarPelicula.getFuncion().get(2)).get(4), ((ArrayList<?>) asignarPelicula.getFuncion().get(2)).get(3).toString(),  ((ArrayList<?>) asignarPelicula.getFuncion().get(2)).get(1).toString());
 
-			}
-		});
-		pelicula1.setBounds(118, 129, 200, 310);
-		contentPane.add(pelicula1);
-		
-		
-		JLabel lblNewLabel = new JLabel(((ArrayList) asignarPelicula.getFuncion().get(0)).get(2).toString());
-		lblNewLabel.setBounds(181, 451, 61, 16);
-		contentPane.add(lblNewLabel);
-		
-		//
-		
-		JButton pelicula2 = new JButton("");
-		pelicula2.addActionListener(new ActionListener() {
+	
+	
+}
+	
+	
+	
+	public void getPeli(Object object, String texto, String posicion) {
+		JButton ButtonPelicula = new JButton("");
+		ButtonPelicula.setIcon(new ImageIcon("/Users/ferney/CineUQ/CineUQ/imagenes/"+object+""));
+		ButtonPelicula.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		pelicula2.setBounds(416, 129, 200, 310);
-		contentPane.add(pelicula2);
-		
-		//
-		
-		
-		JButton pelicula3 = new JButton("");
-		pelicula3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		pelicula3.setBounds(702, 129, 200, 310);
-		contentPane.add(pelicula3);
-		
-		
-		
-		
-		
-		//
-		
-	     }
+				System.out.println(posicion);
+				
 	}
+		});
+		ButtonPelicula.setBounds(59, 129, 250, 310);
+		contentPane.add(ButtonPelicula);
+		JLabel txtPelicula1 = new JLabel( "<html><p>" + texto + "</p></html>");
+		txtPelicula1.setVerticalAlignment(SwingConstants.TOP);
+		if (posicion.equals("0") || posicion.equals("3") || posicion.equals("6")) {
+			ButtonPelicula.setBounds(59, 129, 250, 310);
+			txtPelicula1.setBounds(59, 451, 266, 160);
+		} else if(posicion.equals("1")) {
+			txtPelicula1.setBounds(382, 451, 242, 176);
+			ButtonPelicula.setBounds(382, 129, 242, 310);
+		} else {
+			ButtonPelicula.setBounds(691, 129, 250, 310);
+			txtPelicula1.setBounds(691, 451, 250, 190);
+		}
+		contentPane.add(txtPelicula1);
+    }
+	
+	
+	
+	
+		 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	
+public void getPelicula1() {
+		JButton ButtonPelicula1 = new JButton("");
+		ButtonPelicula1.setIcon(new ImageIcon("/Users/ferney/CineUQ/CineUQ/imagenes/"+((ArrayList<?>) asignarPelicula.getFuncion().get(0)).get(4)+""));
+		ButtonPelicula1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+	}
+		});
+		ButtonPelicula1.setBounds(59, 129, 250, 310);
+		contentPane.add(ButtonPelicula1);
+		JLabel txtPelicula1 = new JLabel( "<html><p>" + ((ArrayList<?>) asignarPelicula.getFuncion().get(0)).get(3).toString()+ "</p></html>");
+		txtPelicula1.setVerticalAlignment(SwingConstants.TOP);
+		txtPelicula1.setBounds(59, 451, 266, 160);
+		contentPane.add(txtPelicula1);
+		
+    }
+		
+	
+		
+		
+		public void getPelicula2() {
+		JButton ButtonPelicula2 = new JButton("");
+		ButtonPelicula2.setIcon(new ImageIcon("/Users/ferney/CineUQ/CineUQ/imagenes/"+((ArrayList<?>) asignarPelicula.getFuncion().get(1)).get(4)+""));
+
+		ButtonPelicula2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		    });
+		ButtonPelicula2.setBounds(382, 129, 242, 310);
+		   
+			JLabel txtPelicula2 = new JLabel("<html><p>" + ((ArrayList<?>) asignarPelicula.getFuncion().get(1)).get(3).toString()+ "</p></html>");
+			txtPelicula2.setVerticalAlignment(SwingConstants.TOP);
+			txtPelicula2.setBounds(382, 451, 242, 176);
+			contentPane.add(txtPelicula2);
+		    
+		    contentPane.add(ButtonPelicula2);
+      }
+	
+		
+		
+		
+		
+		public void getPelicula3() {
+		JButton ButtonPelicula3 = new JButton("");
+		ButtonPelicula3.setIcon(new ImageIcon("/Users/ferney/CineUQ/CineUQ/imagenes/"+((ArrayList<?>) asignarPelicula.getFuncion().get(2)).get(4)+""));
+		ButtonPelicula3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		ButtonPelicula3.setBounds(691, 129, 250, 310);
+		contentPane.add(ButtonPelicula3);
+		
+			JLabel txtPelicula3 = new JLabel("<html><p>" + ((ArrayList<?>) asignarPelicula.getFuncion().get(2)).get(3).toString()+ "</p></html>");
+			txtPelicula3.setVerticalAlignment(SwingConstants.TOP);
+			txtPelicula3.setHorizontalAlignment(SwingConstants.LEFT);
+			txtPelicula3.setBounds(691, 451, 250, 190);
+			contentPane.add(txtPelicula3);
+		
+		
+	 }
+	 
+	 */
+	
+	}
+
+
 
 
