cf96e003d90411ea8c772edcd019f74fa6ebb1e9
joganis <jobanya.giraldos@uqvirtual.edu.co>
2022-10-18 20:07:13 -0500

    Clasificación

--- CineUQ/src/co/uniquindio/programacion1/asignar_Funcion/model/Asignar_Peliculas_Model.java
+++ CineUQ/src/co/uniquindio/programacion1/asignar_Funcion/model/Asignar_Peliculas_Model.java
@@ -8,7 +8,7 @@ public class Asignar_Peliculas_Model {
 	public ArrayList getFuncion () {
 		ArrayList<ArrayList<String>> funciones= new ArrayList<ArrayList<String>>();
 		String laHuerfana = "La Huérfana: El Origen, 120 Min, Clasificacion +15, Sinopsis: La desquiciada Leena Klammer organiza una brillante fuga de un manicomio de Estonia y viaja a Estados Unidos robando la identidad de la hija.";
-		String tadeo = "Tadeo Jones 3, 90 Min, Clasificacion +7, Sinopsis: El sueño más grande de Tad es ser aceptado por sus colegas arqueólogos, pero su naturaleza accidentada se interpone. Cuando él, inadvertidamente, destruye un sarcófago extraño, se conjura un hechizo que pone en peligro la vida de sus amigos.";
+		String tadeo = "Tadeo Jones 3, 90 Min, Clasificacion +jjjjjjjj7, Sinopsis: El sueño más grande de Tad es ser aceptado por sus colegas arqueólogos, pero su naturaleza accidentada se interpone. Cuando él, inadvertidamente, destruye un sarcófago extraño, se conjura un hechizo que pone en peligro la vida de sus amigos.";
 		String superMascotas = "Súper-Mascotas, 110 Min, Clasificacion +7, Sinopsis: Cuando la Liga de la Justicia es capturada por Lex Luthor, el perro de Superman, Krypto, forma un equipo de mascotas de refugio a las que se les otorgan superpoderes.";
 		String tres = "3:00 PM";
 		String cinco = "5:00 PM";
